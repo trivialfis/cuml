3789a9c69a4cdde35c1af6040b8158d6e8aa1e00
fis <jm.yuan@outlook.com>
2022-07-12 07:12:27 +0800

    Remove old code.

--- cpp/include/cuml/manifold/spectral.hpp
+++ cpp/include/cuml/manifold/spectral.hpp
@@ -16,17 +16,5 @@ void fit_embedding(const raft::handle_t& handle,
                    int n_components,
                    float* out,
                    unsigned long long seed);
-
-/**
- * \brief Compute the spectral embeddings given dense input X.
- */
-void fit_embedding(raft::handle_t const& handle,
-                   float* X,
-                   int n_samples,
-                   int n_features,
-                   int n_neighbors,
-                   int n_components,
-                   float* out,
-                   uint64_t seed);
 }  // namespace Spectral
 }  // namespace ML
--- cpp/src/spectral/spectral.cu
+++ cpp/src/spectral/spectral.cu
@@ -16,148 +16,21 @@
 #include <cuml/manifold/common.hpp>
 #include <cuml/manifold/spectral.hpp>
 
-#include <raft/linalg/unary_op.cuh>
+#include <raft/core/mdarray.hpp>
 #include <raft/linalg/transpose.hpp>
+#include <raft/linalg/unary_op.cuh>
 #include <raft/sparse/coo.hpp>
 #include <raft/sparse/linalg/spectral.hpp>
 #include <raft/sparse/linalg/symmetrize.hpp>
 #include <raft/sparse/op/detail/filter.cuh>
 #include <raft/spatial/knn/knn.cuh>
-#include <raft/core/mdarray.hpp>
 
 #include <thrust/iterator/discard_iterator.h>
 #include <thrust/iterator/transform_iterator.h>
 #include <thrust/iterator/transform_output_iterator.h>
 
 namespace ML {
-
 namespace Spectral {
-
-template <typename T>
-MDSPAN_INLINE_FUNCTION auto native_popc(T v) -> int32_t
-{
-  int c = 0;
-  for (; v != 0; v &= v - 1) {
-    c++;
-  }
-  return c;
-}
-
-MDSPAN_INLINE_FUNCTION auto popc(uint32_t v) -> int32_t
-{
-#if defined(__CUDA_ARCH__)
-  return __popc(v);
-#elif defined(__GNUC__) || defined(__clang__)
-  return __builtin_popcount(v);
-#else
-  return native_popc(v);
-#endif  // compiler
-}
-
-MDSPAN_INLINE_FUNCTION auto popc(uint64_t v) -> int32_t
-{
-#if defined(__CUDA_ARCH__)
-  return __popcll(v);
-#elif defined(__GNUC__) || defined(__clang__)
-  return __builtin_popcountll(v);
-#else
-  return native_popc(v);
-#endif  // compiler
-}
-
-template <class T, std::size_t N, std::size_t... Idx>
-MDSPAN_INLINE_FUNCTION constexpr auto arr_to_tup(T (&arr)[N], std::index_sequence<Idx...>)
-{
-  return std::make_tuple(arr[Idx]...);
-}
-
-template <class T, std::size_t N>
-MDSPAN_INLINE_FUNCTION constexpr auto arr_to_tup(T (&arr)[N])
-{
-  return arr_to_tup(arr, std::make_index_sequence<N>{});
-}
-
-// uint division optimization inspired by the CIndexer in cupy.  Division operation is
-// slow on both CPU and GPU, especially 64 bit integer.  So here we first try to avoid 64
-// bit when the index is smaller, then try to avoid division when it's exp of 2.
-template <typename I, size_t... Extents>
-MDSPAN_INLINE_FUNCTION auto unravel_index_impl(I idx, raft::detail::stdex::extents<Extents...> shape)
-{
-  constexpr auto kRank = static_cast<int32_t>(shape.rank());
-  size_t index[shape.rank()]{0};  // NOLINT
-  static_assert(std::is_signed<decltype(kRank)>::value,
-                "Don't change the type without changing the for loop.");
-  for (int32_t dim = kRank; --dim > 0;) {
-    auto s = static_cast<std::remove_const_t<std::remove_reference_t<I>>>(shape.extent(dim));
-    if (s & (s - 1)) {
-      auto t     = idx / s;
-      index[dim] = idx - t * s;
-      idx        = t;
-    } else {  // exp of 2
-      index[dim] = idx & (s - 1);
-      idx >>= popc(s - 1);
-    }
-  }
-  index[0] = idx;
-  return arr_to_tup(index);
-}
-
-/**
- * \brief Turns linear index into coordinate.  Similar to numpy unravel_index. This is not
- *        exposed to public as it's not part of the mdspan proposal, the returned tuple
- *        can not be directly used for indexing into mdspan and we might change the return
- *        type in the future.
- *
- * \code
- *   auto m = make_host_matrix<float>(7, 6);
- *   auto m_v = m.view();
- *   auto coord = detail::unravel_index(2, m.extents(), typename decltype(m)::layout_type{});
- *   detail::apply(m_v, coord) = 2;
- * \endcode
- *
- * \param idx    The linear index.
- * \param shape  The shape of the array to use.
- * \param layout Must be `layout_right` (row-major) in current implementation.
- *
- * \return A thrust::tuple that represents the coordinate.
- */
-template <typename LayoutPolicy, std::size_t... Exts>
-MDSPAN_INLINE_FUNCTION auto unravel_index(size_t idx,
-                                          raft::detail::stdex::extents<Exts...> shape,
-                                          LayoutPolicy const&)
-{
-  static_assert(std::is_same<LayoutPolicy, raft::detail::stdex::layout_right>::value,
-                "Only C layout is supported.");
-  if (idx > std::numeric_limits<uint32_t>::max()) {
-    return unravel_index_impl<uint64_t, Exts...>(static_cast<uint64_t>(idx), shape);
-  } else {
-    return unravel_index_impl<uint32_t, Exts...>(static_cast<uint32_t>(idx), shape);
-  }
-}
-
-template <typename Fn, typename Tup, size_t... I>
-MDSPAN_INLINE_FUNCTION auto constexpr apply_impl(Fn&& f, Tup&& t, std::index_sequence<I...>)
-  -> decltype(auto)
-{
-  return f(thrust::get<I>(t)...);
-}
-
-/**
- * C++ 17 style apply for thrust tuple.
- *
- * \param f function to apply
- * \param t tuple of arguments
- */
-template <typename Fn,
-          typename Tup,
-          std::size_t kTupSize = std::tuple_size<std::remove_reference_t<Tup>>::value>
-MDSPAN_INLINE_FUNCTION auto constexpr apply(Fn&& f, Tup&& t) -> decltype(auto)
-{
-  return apply_impl(
-    std::forward<Fn>(f), std::forward<Tup>(t), std::make_index_sequence<kTupSize>{});
-}
-
-
 template <typename index_type, typename value_type, typename ThrustExecPolicy>
 auto Diagonal(index_type const* rows,
               index_type const* cols,
@@ -208,31 +81,6 @@ cusparseStatus_t cuSparseCreateCOO(cusparseSpMatDescr_t* desc,
                            CUSPARSE_INDEX_32I,
                            CUSPARSE_INDEX_BASE_ZERO,
                            CUDA_R_32F);
-}
-
-template <typename index_t, typename value_t>
-std::string coo_to_str(index_t const* rows,
-                       index_t const* cols,
-                       value_t const* vals,
-                       size_t nnz,
-                       std::string name,
-                       rmm::cuda_stream_view stream)
-{
-  std::vector<index_t> h_rows(nnz);
-  std::vector<index_t> h_cols(nnz);
-  std::vector<value_t> h_vals(nnz);
-  CUDA_CHECK(cudaMemcpyAsync(
-    h_rows.data(), rows, nnz * sizeof(index_t), cudaMemcpyDeviceToHost, stream.value()));
-  CUDA_CHECK(cudaMemcpyAsync(
-    h_cols.data(), cols, nnz * sizeof(index_t), cudaMemcpyDeviceToHost, stream.value()));
-  CUDA_CHECK(cudaMemcpyAsync(
-    h_vals.data(), vals, nnz * sizeof(value_t), cudaMemcpyDeviceToHost, stream.value()));
-
-  std::stringstream ss;
-  for (size_t i = 0; i < h_rows.size(); ++i) {
-    ss << "(" << h_rows[i] << ", " << h_cols[i] << ")\t" << h_vals[i] << "\n";
-  }
-  return ss.str();
 }
 
 template <typename offset_t, typename index_t, typename value_t>
@@ -363,7 +211,6 @@ struct laplacian_matrix_t : public raft::sparse::COO<value_type, index_type> {
             value_type beta,
             value_type* __restrict__ y)
   {
-    CUDA_CHECK(cudaDeviceSynchronize());
     auto cusparse_h = this->handle_.get_cusparse_handle();
     auto stream     = this->handle_.get_stream();
 
@@ -405,9 +252,6 @@ struct laplacian_matrix_t : public raft::sparse::COO<value_type, index_type> {
     CUSPARSE_CHECK(cusparseDestroyDnVec(vec_y));
     CUSPARSE_CHECK(cusparseDestroyDnVec(vec_X));
     CUSPARSE_CHECK(cusparseDestroySpMat(matA));
-
-    CUDA_CHECK(cudaDeviceSynchronize());
-    std::cout << "Finished spmv" << std::endl;
   }
 
   template <typename ThrustExePolicy>
@@ -479,27 +323,17 @@ void Partition(raft::handle_t const& handle,
                     laplacian.vals(),
                     [] HD(weight_t v) { return -v; });
   auto it = thrust::make_counting_iterator(0ul);
-  CUDA_CHECK(cudaDeviceSynchronize());
-
-  // std::cout << coo_view_t<vertex_t, weight_t>{laplacian}.toStr(handle, "Laplacian") << std::endl;
-  // std::cout << raft::arr2Str(laplacian.rows(), laplacian.nnz, "rows", stream) << std::endl;
-  // std::cout << raft::arr2Str(laplacian.cols(), laplacian.nnz, "cols", stream) << std::endl;
-  // std::cout << raft::arr2Str(laplacian.vals(), laplacian.nnz, "vals", stream) << std::endl;
 
   CSR<vertex_t, vertex_t, weight_t> csr(handle, n_samples, n_samples, nnz);
   coo_view_t<vertex_t, weight_t>{laplacian}.toCSR(handle, &csr);
 
   auto r_csr_m = raft::spectral::matrix::sparse_matrix_t<vertex_t, weight_t>(csr);
   eigen_solver.solve_largest_eigenvectors(handle, r_csr_m, eigVals, eigVecs);
-  // thrust::transform(
-  //     policy, eigVecs, eigVecs + n_components * n_samples, eigVecs, [] HD(weight_t v) { return -v; });
-  // raft::spectral::transform_eigen_matrix(handle, policy, n_samples, n_components, eigVecs);
-  // std::cout << raft::arr2Str(eigVals, n_components, "cuml eigen values", stream) << std::endl;
 
   raft::device_matrix_view<float> eig_vectors(eigVecs, n_components, n_samples);
   auto permutation = raft::make_device_matrix<float>(handle, n_components, n_samples);
 
-  it              = thrust::make_counting_iterator(0ul);
+  it          = thrust::make_counting_iterator(0ul);
   auto result = permutation.view();
   thrust::for_each(policy, it, it + n_samples * n_components, [=] __device__(size_t i) {
     size_t ridx        = i / n_samples;
@@ -513,14 +347,6 @@ void Partition(raft::handle_t const& handle,
                              cudaMemcpyDefault,
                              handle.get_stream()));
 
-  // std::cout << raft::arr2Str(eigVecs, n_components * n_samples, "embeddings::", stream)
-  //           << std::endl;
-
-
-  // std::cout << raft::arr2Str(
-  //                laplacian.diagonal_.data(), laplacian.diagonal_.size(), "diagonal", stream)
-  //           << std::endl;
-
   auto d_diagonal = laplacian.diagonal_.data();
   it              = thrust::make_counting_iterator(0ul);
   std::cout << "n_diagonal:" << laplacian.diagonal_.size() << std::endl;
@@ -530,10 +356,6 @@ void Partition(raft::handle_t const& handle,
     eigVecs[i] /= d_diagonal[cidx];
   });
 
-  // std::cout << raft::arr2Str(eigVecs, n_components * n_samples, "normalization::", stream)
-  //           << std::endl;
-
-  CUDA_CHECK(cudaDeviceSynchronize());
   {
     // deterministic vector sign flip
     auto abs_it =
@@ -541,7 +363,7 @@ void Partition(raft::handle_t const& handle,
     auto key_it =
       thrust::make_transform_iterator(it, [=] HD(size_t i) -> size_t { return i % n_samples; });
     auto signs = raft::make_device_vector<float>(n_components, stream);
-    CUDA_CHECK(cudaDeviceSynchronize());
+
     thrust::reduce_by_key(policy,
                           key_it,
                           key_it + n_components * n_samples,
@@ -598,7 +420,6 @@ void fit_embedding(const raft::handle_t& handle,
 
   handle.get_stream().synchronize();
 
-  std::cout << "n_comp:" << n_components << std::endl;
   index_type neigvs       = n_components + 1;
   index_type maxiter      = 5000;  // default reset value (when set to 0);
   value_type tol          = 0.001;
@@ -623,20 +444,9 @@ void fit_embedding(const raft::handle_t& handle,
                                     eigen_values.data(),
                                     eigen_vectors.data());
 
-  // std::cout << raft::arr2Str(eigen_vectors.data(), eigen_vectors.size(), "eig vectors::", stream)
-  //           << std::endl;
-
-  // drop first
-  // raft::linalg::transpose(
-  //   handle, eigen_vectors.data(), tran.data(), neigvs, n, handle.get_stream());
-  // std::cout << raft::arr2Str(tran.data(), tran.size(), "trans::", stream)
-  //           << std::endl;
-
-  // auto tran_v = tran.view();
-  auto map_v = raft::make_device_matrix_view(eigen_vectors.data(), neigvs, n);
+  auto map_v      = raft::make_device_matrix_view(eigen_vectors.data(), neigvs, n);
   auto drop_first = raft::detail::stdex::submdspan(
     map_v, std::make_tuple(1, neigvs), raft::detail::stdex::full_extent);
-  std::cout << "extent:" << drop_first.extent(0) << ", " << drop_first.extent(1) << std::endl;
   ASSERT(drop_first.size() == static_cast<size_t>(n_components * n), "Invalid shape of eigen map.");
 
   auto tran   = raft::make_device_matrix<float>(n_components, n, handle.get_stream());
@@ -665,100 +475,6 @@ void fit_embedding(const raft::handle_t& handle,
                              out,
                              n_components));
   }
-}
-
-void fit_embedding_with_knn(raft::handle_t const& handle,
-                            int n,
-                            int32_t* knn_indices,
-                            float* knn_dists,
-                            int n_components,
-                            int n_neighbors,
-                            float* out,
-                            uint64_t seed)
-{
-  using value_t   = float;
-  using value_idx = int32_t;
-  raft::sparse::COO<value_t, value_idx> knn_coo(handle.get_stream());
-
-  // fixme: is this necessary?
-  raft::sparse::linalg::from_knn_symmetrize_matrix<value_idx, value_t>(
-    knn_indices, knn_dists, n, n_neighbors, &knn_coo, handle.get_stream());
-  raft::sparse::COO<value_t, value_idx> out_coo(handle.get_stream());
-  raft::sparse::op::detail::coo_remove_zeros<256, value_t>(&knn_coo, &out_coo, handle.get_stream());
-  raft::linalg::unaryOp(
-    out_coo.vals(),
-    out_coo.vals(),
-    out_coo.nnz,
-    [] __device__(float v) { return v * 0.5; },
-    handle.get_stream());
-  // std::cout << raft::arr2Str(out_coo.vals(), out_coo.nnz, "symmetrized valus",
-  //                            handle.get_stream())
-  //           << std::endl;
-
-  raft::sparse::op::coo_sort<value_t>(&out_coo, handle.get_stream());
-
-  raft::sparse::spectral::fit_embedding(handle,
-                                        /*rows=*/out_coo.rows(),
-                                        /*cols=*/out_coo.cols(),
-                                        /*vals=*/out_coo.vals(),
-                                        /*nnz=*/out_coo.nnz,
-                                        /*n=*/(value_idx)n,
-                                        n_components,
-                                        out,
-                                        seed);
-}
-
-void fit_embedding(raft::handle_t const& handle,
-                   float* X,
-                   int n_samples,
-                   int n_features,
-                   int n_neighbors,
-                   int n_components,
-                   float* out,
-                   uint64_t seed)
-{
-  manifold_dense_inputs_t<float> inputs(X, nullptr, n_samples, n_features);
-
-  rmm::device_uvector<knn_indices_dense_t> knn_indices(n_samples * n_neighbors,
-                                                       handle.get_stream());
-  rmm::device_uvector<float> knn_dists(n_samples * n_neighbors, handle.get_stream());
-  knn_graph<knn_indices_dense_t, float> knn(
-    n_samples, n_neighbors, knn_indices.data(), knn_dists.data());
-
-  std::vector<float*> ptrs(1, inputs.X);
-  std::vector<int> sizes(1, inputs.n);
-
-  raft::spatial::knn::brute_force_knn(handle,
-                                      ptrs,
-                                      sizes,
-                                      n_features,
-                                      inputs.X,
-                                      inputs.n,
-                                      knn.knn_indices,
-                                      knn.knn_dists,
-                                      n_neighbors);
-
-  std::string indices_str =
-    raft::arr2Str(knn_indices.data(), knn_indices.size(), "knn_indices", handle.get_stream());
-  std::string values_str =
-    raft::arr2Str(knn_dists.data(), knn_dists.size(), "knn_distances", handle.get_stream());
-
-  // std::pair<int32_t, int32_t> shape;
-  // shape.first  = n_samples;
-  // shape.second = n_samples;
-
-  rmm::device_uvector<int> knn_indices_i32(n_samples * n_neighbors, handle.get_stream());
-  thrust::copy_n(rmm::exec_policy(handle.get_stream()),
-                 knn.knn_indices,
-                 n_samples * n_neighbors,
-                 knn_indices_i32.begin());
-
-  // std::cout << raft::arr2Str(knn_indices_i32.data(), knn_indices_i32.size(),
-  //                            "knn_indices", handle.get_stream())
-  //           << std::endl;
-
-  fit_embedding_with_knn(
-    handle, n_samples, knn_indices_i32.data(), knn.knn_dists, n_components, n_neighbors, out, seed);
 }
 }  // namespace Spectral
 }  // namespace ML
